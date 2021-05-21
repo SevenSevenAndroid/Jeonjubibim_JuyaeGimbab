dacafbed480bb39a243e355b9e0fa87a2932d191
JooYae <jooyae3970@naver.com>
2021-05-21 13:36:13 +0900

    DI : appmodule

---
+++ app/src/main/java/org/sopt/androidseminar/AppModule.kt
@@ -0,0 +1,12 @@
+package org.sopt.androidseminar
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.androidseminar.api.sopt.SoptApi
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
