7da070225081d19a970a4e0b17088f9d88495aa1
JooYae <jooyae3970@naver.com>
2021-06-06 18:57:43 +0900

    [FEAT] SplashView with Animation

---
+++ app/src/main/java/org/sopt/androidseminar/SplashActivity.kt
@@ -0,0 +1,32 @@
+package org.sopt.androidseminar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import org.sopt.androidseminar.databinding.ActivitySplashBinding
+import org.sopt.androidseminar.signin.view.SignInActivity
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        startSplash()
+    }
+
+    private fun startSplash(){
+        Handler().postDelayed({
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, DURATION)
+
+    }
+
+    companion object {
+        private const val DURATION : Long= 5000
+    }
+}
