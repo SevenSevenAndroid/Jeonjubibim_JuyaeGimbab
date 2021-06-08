591f912880371bc67cd30b48bd62b85acfdd743f
JooYae <jooyae3970@naver.com>
2021-06-09 01:24:25 +0900

    [FEAT] Firebase Facebook Login

--- app/src/main/java/org/sopt/androidseminar/signin/view/SignInActivity.kt
+++ app/src/main/java/org/sopt/androidseminar/signin/view/SignInActivity.kt
@@ -1,31 +1,33 @@
 package org.sopt.androidseminar.signin.view
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import okhttp3.ResponseBody
-import org.json.JSONObject
-import org.sopt.androidseminar.signin.dto.ResponseLoginData
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.*
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailabilityLight
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.R
 import org.sopt.androidseminar.api.sopt.ServiceCreator
 import org.sopt.androidseminar.databinding.ActivitySignInBinding
-import org.sopt.androidseminar.signin.dto.RequestLoginData
 import org.sopt.androidseminar.home.view.HomeActivity
+import org.sopt.androidseminar.signin.dto.RequestLoginData
+import org.sopt.androidseminar.signin.dto.ResponseLoginData
 import org.sopt.androidseminar.signin.dto.SoptUserAuthStorage
 import org.sopt.androidseminar.signin.dto.SoptUserInfo
 import org.sopt.androidseminar.signup.SignUpActivity
 import org.sopt.androidseminar.utils.enqueueUtil
 import org.sopt.androidseminar.utils.showToast
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class SignInActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignInBinding
-
+    private var callbackManager: CallbackManager? = null
     private val signUpActivityLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) {
@@ -36,10 +38,34 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
+        callbackManager = CallbackManager.Factory.create()
+
+        FacebookSdk.sdkInitialize(getApplicationContext())
+        AppEventsLogger.activateApp(this)
+
+        FacebookSdk.setApplicationId(getString(org.sopt.androidseminar.R.string.facebook_app_id))
+        FacebookSdk.sdkInitialize(this)
+
+        if (BuildConfig.DEBUG) {
+            FacebookSdk.setIsDebugEnabled(true)
+            FacebookSdk.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS)
+        }
         setContentView(binding.root)
         showSignup()
         setButtonEvent()
         searchUserAuthStorage()
+        facebookLogin()
+        checkgooglePlayServices()
+    }
+    private fun checkgooglePlayServices() : Boolean{
+        val status =  GoogleApiAvailabilityLight.getInstance().isGooglePlayServicesAvailable(this)
+        return if (status != ConnectionResult.SUCCESS) {
+            Log.e("error","error")
+            false
+        }  else {
+            Log.e("updated", "updated")
+            true
+        }
     }
 
     fun showSignup(){
@@ -99,6 +125,42 @@ class SignInActivity : AppCompatActivity() {
             Intent(this, SignUpActivity::class.java)
         )
     }
+
+    private fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, listOf("email", "public_profile"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    if (result?.accessToken != null) {
+                        // facebook 계정 정보를 firebase 서버에게 전달(로그인)
+                        val accessToken = result.accessToken
+                        firebaseAuthWithFacebook(result?.accessToken)
+                    } else {
+                        Log.d("Facebook", "Fail Facebook Login")
+                    }
+                }
+
+                override fun onCancel() {
+                    //취소가 된 경우 할일
+                }
+
+                override fun onError(error: FacebookException?) {
+                    //에러가 난 경우 할일
+                }
+            })
+    }
+
+    private fun firebaseAuthWithFacebook(accessToken: AccessToken?) {
+        // AccessToken 으로 Facebook 인증
+        val credential = FacebookAuthProvider.getCredential(accessToken?.token!!)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
     companion object {
         private const val SIGN_UP_RESULT_CODE = 100
     }
